cca8950bd071aa1f0b55b8777aa6943b5ea0ec63
Brandon Lu <sfxninja183@hotmail.com>
2018-10-30 18:24:12 +1030

    Scene Navigation
    Created basic scene navigation
    Created basic UI Elements

---
+++ Assets/Scripts/Navigation/FromSplash.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FromSplash : MonoBehaviour {
+
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//on any input, load the main menu
+		if (Input.anyKey) {
+			Debug.Log("Loading Main Menu...");
+			SceneManager.LoadScene(1);
+			Debug.Log("Done");
+		}
+	}
+}
---
+++ Assets/Scripts/Navigation/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void LoadScene() {
+		Debug.Log("Loading Scene: \"Lv1_Blue\"");
+		SceneManager.LoadScene(2);
+		Debug.Log("Done");
+	}
+}
